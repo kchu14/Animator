0e4264360223f6b0936b6d392cae52e5d7a50524
kchu14 <kchuinbox@gmail.com>
2018-11-08 15:03:03 -0500

    added files

--- Animator/test/cs3500/animator/model/MotionTest.java
+++ Animator/test/cs3500/animator/model/MotionTest.java
@@ -11,7 +11,7 @@ public class MotionTest {
   public void testGetTextResult() {
     Motion m = new Motion("R", "rectangle", 1, 200, 200, 50, 100,
             new Color(255, 0 ,0), 10, 200, 200, 50, 100, new Color(255, 0 , 0));
-    assertEquals(m.getTextResult(), "motion R 1  200 200 50 100 255 0  0    10  200 200 50 100 255 0  0");
+    assertEquals("motion R 1 200 200 50 100 255 0 0 10 200 200 50 100 255 0 0 \n", m.getTextResult());
   }
 
   @Test
@@ -20,9 +20,9 @@ public class MotionTest {
             new Color(255, 0 ,0), 10, 200, 200, 50, 200, new Color(255, 0 , 0));
     Motion later = new Motion("R", "rectangle", 20, 200, 200, 50, 100,
             new Color(255, 0 ,0), 300, 200, 200, 50, 200, new Color(255, 0 , 0));
-    assertEquals(earlier.compareTo(later), 1);
-    assertEquals(later.compareTo(earlier), -1);
-    assertEquals(earlier.compareTo(earlier), 0);
+    assertEquals(-19, earlier.compareTo(later));
+    assertEquals( 19, later.compareTo(earlier));
+    assertEquals(0, earlier.compareTo(earlier));
   }
 
   @Test
@@ -30,7 +30,7 @@ public class MotionTest {
     //height is changed (should go up by 10 after one tick)
     Motion m = new Motion("R", "rectangle", 1, 200, 200, 50, 100,
             new Color(255, 0 ,0), 10, 200, 200, 50, 200, new Color(255, 0 , 0));
-    assertEquals(m.executeMotion(), new SimpleShape("R", "rectangle", 200,
-            200, 50, 110, new Color(255, 0 ,0)));
+    assertEquals(new SimpleShape("R", "rectangle", 200,
+            200, 50, 110, new Color(255, 0 ,0)),m.executeMotion());
   }
 }
