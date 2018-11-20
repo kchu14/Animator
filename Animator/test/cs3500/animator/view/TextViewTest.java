bad431b695fb8eb6fc1eeaa1f6d181b4d1a2de96
Brandon Klein <33870950+Bklein19@users.noreply.github.com>
2018-11-19 22:39:30 -0500

    fixed text tests

--- Animator/test/cs3500/animator/view/TextViewTest.java
+++ Animator/test/cs3500/animator/view/TextViewTest.java
@@ -1,4 +1,4 @@
-/*
+
 package cs3500.animator.view;
 
 import static org.junit.Assert.assertEquals;
@@ -12,7 +12,7 @@ public class TextViewTest {
   @Test
   public void testTextOutput() {
     TextView view = new TextView("asdfasdf");
-    AnimatorModel m = new AnimatorModelImpl.Builder().declareShape("r", "rectangle")
+    ReadOnlyModel m = new ReadOnlyModel(new AnimatorModelImpl.Builder().declareShape("r", "rectangle")
         .addMotion("r", 0, 100, 100, 50, 50, 0, 0, 255,
             10, 100, 100, 50, 50, 255, 0, 0)
         .addMotion("r", 10, 100, 100, 50, 50, 255, 0, 0,
@@ -20,7 +20,7 @@ public class TextViewTest {
         .addMotion("r", 20, 200, 200, 50, 50, 255, 0, 0,
             30, 200, 200, 150, 150, 255, 0, 0)
         .addMotion("r", 30, 200, 200, 150, 150, 255, 0, 0,
-            40, 200, 200, 150, 150, 255, 0, 0).build();
+            40, 200, 200, 150, 150, 255, 0, 0).build());
     view.playAnimation(m);
     assertEquals(
         "Shape r rectangle\n"
@@ -35,8 +35,8 @@ public class TextViewTest {
   @Test(expected = IllegalArgumentException.class)
   public void testTextOutputNoAnimation() {
     AnimatorView view = new TextView("asdfasdf");
-    AnimatorModel m = new AnimatorModelImpl.Builder().build();
+    ReadOnlyModel m = new ReadOnlyModel(new AnimatorModelImpl.Builder().build());
     view.playAnimation(m);
   }
 
-}*/
+}
